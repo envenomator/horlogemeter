cc7f1ac085802f4754c9f8553eb08789de09cf71
Jeroen Venema <jeroen.venema@gmail.com>
2021-05-24 14:54:25 +0200

    Different average method complete

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,12 @@
 /*
 Horlogemeter
 
-Counts 31.768Hz reference clock on pin PD5/T1
+Counts 32.768Hz reference clock on pin PD5/T1
 While a mechanical watch amplifier generates tick interrupts to pin PD1
-Every interrupt tick, an array of 4/6/10 tick is updated, to calculate the signal validity, and the T1 counter is reset to 0
+Every interrupt tick, an array of 5/6/10 tick is updated, to calculate the signal validity, and the T1 counter is reset to 0
 
-the running sensor4/6/10 arrays note 10 values of the sensor
-Given a tick4 watch, it takes two (2) seconds for the array to display 100% sensor value, which seems ok at this point
 
 */
-// ------- Preamble -------- //
 #include <avr/io.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
@@ -18,38 +15,40 @@ Given a tick4 watch, it takes two (2) seconds for the array to display 100% sens
 #include <LiquidCrystal_I2C.h>
 
 #define TICKSAMPLES 10
-#define SENSORSAMPLES 10
-#define AVGSETTLE 10
-#define AVGLENGTHSTART 60.0
-#define AVGLENGTHMEDIUM 512.0
+#define MAXSIGNALLOSS 5
 
-struct tickavg {
-  double tick5;
-  double tick6;
-  double tick10;
+#define TICK10LOW   3244
+#define TICK10HIGH  3309
+#define TICK6LOW    5188
+#define TICK6HIGH   5734
+#define TICK5LOW    6225
+#define TICK5HIGH   6880
+
+struct tick_struct {
+  uint32_t pulsecount5;   // counts pulses at 32768/s rate
+  uint32_t pulsecount6;
+  uint32_t pulsecount10;
+  uint32_t tickcount5;
+  uint32_t tickcount6;
+  uint32_t tickcount10;
+  uint8_t sensor5[TICKSAMPLES];
+  uint8_t sensor6[TICKSAMPLES];
+  uint8_t sensor10[TICKSAMPLES];
 };
 
-struct ticksums {
-  uint32_t tick5count;
-  uint32_t tick6count;
-  uint32_t tick10count;
-};
-
+// globals used during interrupt routine
+volatile tick_struct ticks; 
 volatile uint8_t tickpointer; // points to item in arrray of tick values
-volatile ticksums sum;        // contains sums for all ticks
-volatile uint8_t sensor5[TICKSAMPLES];
-volatile uint8_t sensor6[TICKSAMPLES];
-volatile uint8_t sensor10[TICKSAMPLES];
 volatile uint16_t ptt; // previously read tick time;
+
+// globals used during loop
+uint32_t tickcount, pulsecount; // used for temp reading respective counters during loop() to avoid race conditions
 uint8_t quality;
 uint8_t temp5, temp6, temp10, i;
-uint8_t ticks = 0;
-uint8_t counter = 0;
-uint16_t maxsignalloss = 0, signalacquired = 0;
-tickavg avg;
+uint8_t current_ticks = 0;
+uint16_t maxsignalloss = 0;
+
 double secperday;
-double averageticktime;
-double avglength;   // length of the EWMA running average (16/64/128 etc)
 LiquidCrystal_I2C lcd(0x3f,20,4);
 char displaytext[21]; 
 
@@ -60,20 +59,19 @@ void clearSensorArrays(void) {
     // zero-out sensor arrays
     for(i = 0; i < TICKSAMPLES; i++)
     {
-        sensor5[i] = 0;
-        sensor6[i] = 0;
-        sensor10[i] = 0;
+        ticks.sensor5[i] = 0;
+        ticks.sensor6[i] = 0;
+        ticks.sensor10[i] = 0;
     }
-    // reset sensor values
+    // reset total pulse counts per tick
+    ticks.pulsecount5 = 0;
+    ticks.pulsecount6 = 0;
+    ticks.pulsecount10 = 0;
 
-    sum.tick5count = 0;
-    sum.tick6count = 0;
-    sum.tick10count = 0;
-
-    avglength = AVGLENGTHSTART;
-    avg.tick5 = avglength*32768.0/5;
-    avg.tick6 = avglength*32768.0/6;
-    avg.tick10 = avglength*32768.0/10;
+    // reset number of counted ticks
+    ticks.tickcount5 = 0;
+    ticks.tickcount6 = 0;
+    ticks.tickcount10 = 0;
 }
 
 void initCounter1(void)
@@ -84,61 +82,65 @@ void initCounter1(void)
     TCNT1 = 0; // reset counter immediately, perhaps not needed
 }
 
-void interrupt0() {         /* Run every time there is a change on button */
+void interrupt0() {        
+    // fires each time a watch tick is detected
+    // need to figure out if this was a valid tick time (after the previous)
+    // and which ticksignal this is (5/6/10 ticks per second)
+    // first tick will probably not count as valid signal, automatically corrected at next tick
+
     ptt = TCNT1;                        // read tick timer
     TCNT1 = 0; // reset tick timer
 
-    if((3244 < ptt) && (ptt < 3309))
+    // asume no signal for this iteration at 'tickpointer'
+    ticks.sensor5[tickpointer] = 0;
+    ticks.sensor6[tickpointer] = 0;
+    ticks.sensor10[tickpointer] = 0;
+
+    if((TICK10LOW < ptt) && (ptt < TICK10HIGH))
     {
-        // Tick 10 found with +/- 5% margin
-        avg.tick10 = ptt + avg.tick10 - ((avg.tick10) / avglength);
-        sensor10[tickpointer] = 10;
-        sum.tick10count++;
+        // Tick 10 found
+        ticks.tickcount10++;
+        ticks.pulsecount10 += ptt;
+        ticks.sensor10[tickpointer] = 10;
     }
     else
     {
-        sensor10[tickpointer] = 0;
-        if((5188 < ptt) && (ptt < 5734))
+        if((TICK6LOW < ptt) && (ptt < TICK6HIGH))
         {
-            // Tick 6 found with +/- 5% margin
-            avg.tick6 = ptt + avg.tick6 - ((avg.tick6) / avglength);
-            sensor6[tickpointer] = 10;
-            sum.tick6count++;
+            // Tick 6 found
+            ticks.tickcount6++;
+            ticks.pulsecount6 += ptt;
+            ticks.sensor6[tickpointer] = 10;
         }
         else
         {
-            sensor6[tickpointer] = 0;
-            if((6225 < ptt) && (ptt < 6880))
+            if((TICK5LOW < ptt) && (ptt < TICK5HIGH))
             {
-                // Tick 5 found with +/- 5% margin
-                avg.tick5 = ptt + avg.tick5 - ((avg.tick5) / avglength);
-                sensor5[tickpointer] = 10;
-                sum.tick5count++;
+                // Tick 5 found
+                ticks.tickcount5++;
+                ticks.pulsecount5 += ptt;
+                ticks.sensor5[tickpointer] = 10;
             }
-            else sensor5[tickpointer] = 0;
         }
     }
+    // next iteration to indicate change in signal quality
     tickpointer++;
     if(tickpointer > TICKSAMPLES-1) tickpointer = 0; // rollover
 }
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   clearSensorArrays();
   initCounter1();
   lcd.init();
   lcd.backlight();
   lcd.clear();
-//  initInterrupt0();
   
-  Serial.println();
-  Serial.println("== Sensor readings ==");
   lcd.printf("+------------------+");
   lcd.setCursor(0,1);
   lcd.printf("|   Horlogemeter   |");
   lcd.setCursor(0,2);
-  lcd.printf("|   Versie 1.2     |");
+  lcd.printf("|   Versie 1.3     |");
   lcd.setCursor(0,3);
   lcd.printf("+------------------+");
   delay(1000);
@@ -146,7 +148,6 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
   delay(1000);
 
@@ -156,111 +157,76 @@ void loop() {
   temp10 = 0;
   for(i = 0; i < TICKSAMPLES; i++)
   {
-    temp5 += sensor5[i];
-    temp6 += sensor6[i];
-    temp10 += sensor10[i];
+    temp5 += ticks.sensor5[i];
+    temp6 += ticks.sensor6[i];
+    temp10 += ticks.sensor10[i];
   }
   if(temp5 >= 50)
   {
-    ticks = 5;
+    current_ticks = 5;
     quality = temp5;
-    secperday = (avg.tick5/avglength*5*3600*24/32768)-86400;
-    averageticktime = avg.tick5/avglength;
+    // fast read to avoid race condition
+    pulsecount = ticks.pulsecount5;
+    tickcount  = ticks.tickcount5;
   }
   else
   {
     if(temp6 >= 50)
     {
-        ticks = 6;
+        current_ticks = 6;
         quality = temp6;
-        secperday = (avg.tick6/avglength*6*3600*24/32768)-86400;
-        averageticktime = avg.tick6/avglength;
+        // fast read to avoid race condition
+        pulsecount = ticks.pulsecount6;
+        tickcount  = ticks.tickcount6;
     }
     else
     {
         if(temp10 >= 50)
         {
-            ticks = 10;
+            current_ticks = 10;
             quality = temp10;
-            secperday = (avg.tick10/avglength*10*3600*24/32768)-86400;
-            averageticktime = avg.tick10/avglength;
+            // fast read to avoid race condition
+            pulsecount = ticks.pulsecount10;
+            tickcount  = ticks.tickcount10;
         }
         else 
         {
-          ticks = 0;
+          current_ticks = 0;
           quality = 0;
         }
     }
   }
-  if(ticks == 0)
+  if(current_ticks == 0)
   {
     lcd.clear(); 
     lcd.setCursor(4,1);
     lcd.printf("Geen signaal");
-    Serial.printf("Run %3d - No watch signal detected\r\n",counter);
     maxsignalloss += 1; // signal lost in this period, set counter
-    if(maxsignalloss > 5)
+    if(maxsignalloss > MAXSIGNALLOSS)
     {
       // reset all counters and re-acquire signal
       clearSensorArrays(); // reset all sensors for re-acquiry
-      //Serial.printf("Run %3d - Resetting counters\r\n", counter);
       maxsignalloss = 0;
-      signalacquired = 0;
-      avglength = AVGLENGTHSTART;
     }
   }
   else
   {
-    // we have a signal, test how many times we got it first
-    signalacquired += 1;
+    // valid signal and average, calculate and print out results
+    // calculate delta pulses per tick first (expected # pulses - pulsecount) / tickcount
+    // then extrapolate to seconds per day
+    secperday = ((((tickcount * 32768/current_ticks) - pulsecount) / tickcount)*3600*24/32768)-86400;
 
-    if(signalacquired > AVGSETTLE)
-    {
-      // valid signal and average, print out results
-      lcd.clear();
-      snprintf(displaytext,sizeof(displaytext),"Signaal %d%%",quality);
-      lcd.print(displaytext);
-      lcd.setCursor(0,1);
-      snprintf(displaytext,sizeof(displaytext),"%d t/s - %3.2f ms/t",ticks, averageticktime*(1000.0/32768));
-      lcd.print(displaytext);
-      lcd.setCursor(0,2);
-      lcd.printf("%+06.1f sec/dag",secperday);
-      lcd.setCursor(0,3);
-      if(secperday > 0) lcd.printf("             Te snel");
-      else lcd.printf("         Te langzaam");
-      Serial.printf("Run %3d - %1d ticks/s - Signal %3d%% - average %4.2f = %3.2f sec/day\r\n",counter,ticks,quality,averageticktime,secperday);
-      maxsignalloss = 0; // signal present, so reset counter
-
-      // check if the average needs an additional length after the start
-      if((signalacquired > (uint16_t)AVGLENGTHSTART) && avglength == AVGLENGTHSTART)
-      {
-        // switch to higher length, average out more samples
-        avglength = AVGLENGTHMEDIUM;
-        avg.tick5 = (AVGLENGTHMEDIUM/AVGLENGTHSTART)*avg.tick5;
-        avg.tick6 = (AVGLENGTHMEDIUM/AVGLENGTHSTART)*avg.tick6;
-        avg.tick10 = (AVGLENGTHMEDIUM/AVGLENGTHSTART)*avg.tick10;
-        signalacquired = (uint16_t)AVGSETTLE;
-        Serial.print("Switching to higher average length\r\n");
-      }
-    }
-    else
-    {
-      // too few signals, let's wait for the average to clear up a little
-      lcd.clear();
-      snprintf(displaytext,sizeof(displaytext),"Signaal %d%%",quality);
-      lcd.print(displaytext);
-      lcd.setCursor(0,1);
-      snprintf(displaytext,sizeof(displaytext),"%d tikken/s",ticks);
-      lcd.print(displaytext);
-      lcd.setCursor(0,2);
-      lcd.printf("Gemiddelde bepalen");
-      lcd.setCursor(0,3);
-      snprintf(displaytext,sizeof(displaytext),"%d van %d samples", signalacquired, AVGSETTLE);
-      lcd.printf(displaytext);
-      Serial.printf("Run %3d - Signal %3d%% acquired, %d ticks/s - settling averages\r\n", counter, quality, ticks);
-    }
-    
-  }   //Serial.printf("%ld %d %d %ld %f %1.2f\r\n",sum.tick5count, ticks, ptt, average.tick5>>8, avg.tick5/1024.0, (avg.tick5/1024.0*5*3600*24/32768)-86400);
-  
-  counter++;
+    lcd.clear();
+    snprintf(displaytext,sizeof(displaytext),"Signaal %d%%",quality);
+    lcd.print(displaytext);
+    lcd.setCursor(0,1);
+    snprintf(displaytext,sizeof(displaytext),"%d t/s",current_ticks);
+    lcd.print(displaytext);
+    lcd.setCursor(0,2);
+    lcd.printf("%+06.1f sec/dag",secperday);
+    lcd.setCursor(0,3);
+    if(secperday > 0) lcd.printf("             Te snel");
+    else lcd.printf("         Te langzaam");
+    maxsignalloss = 0; // signal present, so reset counter
+  }   
 }
